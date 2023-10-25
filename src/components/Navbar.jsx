b405efdb14f616a909c3237cdc5fcbd73fa83e61
ecpieritz <ecpieritz@gmail.com>
2023-10-25 14:13:25 -0300

    Added scss to Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import logoImg from "../assets/img/ny-logo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 function Navbar() {
+  const [isCollapsed, setIsCollapsed] = useState(false);
+
+  const toggleCollapse = () => {
+    setIsCollapsed(!isCollapsed);
+  };
+
   return (
-    <div class="fixed-top">
-      <nav class="navbar navbar-light bg-light">
-        <a href="/">
-          <img src={logoImg} alt="Namaste logo" />
-        </a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#ny-navbar-id"
-          aria-controls="ny-navbar-id"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
+    <div className="fixed-top ny-navbar ny-bg__milk">
+      <div className="container-fluid">
+        <nav className="navbar navbar-light">
+          <a href="/">
+            <img src={logoImg} alt="Namaste logo" />
+          </a>
+          <button
+            className={`navbar-toggler${isCollapsed ? " collapsed" : ""}`}
+            type="button"
+            data-toggle="collapse"
+            data-target="#ny-navbar-id"
+            aria-controls="ny-navbar-id"
+            aria-expanded={isCollapsed ? "true" : "false"}
+            aria-label="Toggle navigation"
+            onClick={toggleCollapse}
+          >
+            <FontAwesomeIcon icon={faBars} />
+          </button>
+        </nav>
+        <div
+          className={`collapse${isCollapsed ? " show" : ""}`}
+          id="ny-navbar-id"
         >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-      </nav>
-      <div class="collapse" id="ny-navbar-id">
-        <div class="bg-light p-4 text-right">
-              <Link to="/"><p>Home</p></Link>
-              <Link to="/about"><p>About</p></Link>
-              <Link to="/videos"><p>Videos</p></Link>
-              <Link to="/contact"><p>Contact</p></Link>
+          <div className="p-4 text-right ny-navbar__links">
+            <Link to="/" onClick={toggleCollapse}>
+              <p>Home</p>
+            </Link>
+            <Link to="/about" onClick={toggleCollapse}>
+              <p>About</p>
+            </Link>
+            <Link to="/videos" onClick={toggleCollapse}>
+              <p>Videos</p>
+            </Link>
+            <Link to="/contact" onClick={toggleCollapse}>
+              <p>Contact</p>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
