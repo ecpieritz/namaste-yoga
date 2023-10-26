5b460101b35ca3d9aa1f7aea1d50dbf7a5591fb3
ecpieritz <ecpieritz@gmail.com>
2023-10-26 12:31:52 -0300

    Changed link

--- src/components/Home/HomeDiscover.jsx
+++ src/components/Home/HomeDiscover.jsx
@@ -5,7 +5,7 @@ function HomeDiscover() {
     <section className='ny-home__discover'>
       <div className="container-fluid">
         <h2>Discover yourself<br/>through yoga</h2>
-        <a className="ny-btn beige-bg" href="/videos">Watch Our Online Courses</a>
+        <a className="ny-btn beige-bg" href="/contact">Start Your Journey</a>
       </div>
     </section>
   )
