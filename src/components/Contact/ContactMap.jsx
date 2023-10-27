c260c0627bc1bb356d41af037522e7e3fc5992e9
ecpieritz <ecpieritz@gmail.com>
2023-10-27 12:45:34 -0300

    Added Contact Map section

---
+++ src/components/Contact/ContactMap.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function ContactMap() {
+  return (
+    <section className="ny-contact__map">
+      <iframe
+        title="AnanindeuaMap"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63819.11752055811!2d-48.42818215618465!3d-1.3597310895072812!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x92a45fe447932a59%3A0xf3812b35573454d3!2sAnanindeua%2C%20PA!5e0!3m2!1spt-BR!2sbr!4v1698421333926!5m2!1spt-BR!2sbr"
+        width="100%"
+        height="600"
+        allowfullscreen=""
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"
+      ></iframe>
+    </section>
+  );
+}
+
+export default ContactMap;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ContactForm from '../components/ContactForm'
+import ContactMap from '../components/Contact/ContactMap'
 
 function Contact() {
   return (
     <main className='ny-contact'>
       <ContactForm />
+      <ContactMap />
     </main>
   )
 }
